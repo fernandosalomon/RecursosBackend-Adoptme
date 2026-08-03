093527ed544f3ce581a4df4ab4956dc7bf57b987
fernandosalomon <fernandosalomon1992@gmail.com>
2026-08-03 10:58:01 -0300

    feat: add try-catch to adoption.controller.js

--- src/controllers/adoptions.controller.js
+++ src/controllers/adoptions.controller.js
@@ -5,50 +5,74 @@ import {
 } from "../services/index.js";
 
 const getAllAdoptions = async (req, res) => {
-  const result = await adoptionsService.getAll();
-  res.send({ status: "success", payload: result });
+  try {
+    const result = await adoptionsService.getAll();
+    res.send({ status: "success", payload: result });
+  } catch (error) {
+    res.status(500).send({
+      status: "error",
+      message: `Internal Server Error: ${error}`,
+      payload: [],
+    });
+  }
 };
 
 const getAdoption = async (req, res) => {
   const adoptionId = req.params.aid;
-  const adoption = await adoptionsService.getBy({ _id: adoptionId });
-  if (!adoption)
-    return res
-      .status(404)
-      .send({ status: "error", error: "Adoption not found" });
-  res.send({ status: "success", payload: adoption });
+  try {
+    const adoption = await adoptionsService.getBy({ _id: adoptionId });
+    if (!adoption)
+      return res
+        .status(404)
+        .send({ status: "error", error: "Adoption not found" });
+    res.send({ status: "success", payload: adoption });
+  } catch (error) {
+    res.status(500).send({
+      status: "error",
+      message: `Internal Server Error: ${error}`,
+      payload: [],
+    });
+  }
 };
 
 const createAdoption = async (req, res) => {
   const { uid, pid } = req.params;
-  const user = await usersService.getUserById(uid);
-  if (!user)
-    return res
-      .status(404)
-      .send({ status: "error", message: "User not found", payload: [] });
-  const pet = await petsService.getBy({ _id: pid });
-  if (!pet)
-    return res
-      .status(404)
-      .send({ status: "error", message: "Pet not found", payload: [] });
-  if (pet.adopted)
-    return res.status(400).send({
+  try {
+    const user = await usersService.getUserById(uid);
+    if (!user)
+      return res
+        .status(404)
+        .send({ status: "error", message: "User not found", payload: [] });
+    const pet = await petsService.getBy({ _id: pid });
+    if (!pet)
+      return res
+        .status(404)
+        .send({ status: "error", message: "Pet not found", payload: [] });
+    if (pet.adopted)
+      return res.status(400).send({
+        status: "error",
+        message: "Pet is already adopted",
+        payload: [],
+      });
+    user.pets.push(pet._id);
+    await usersService.update(user._id, { pets: user.pets });
+    await petsService.update(pet._id, { adopted: true, owner: user._id });
+    const newAdoption = await adoptionsService.create({
+      owner: user._id,
+      pet: pet._id,
+    });
+    res.status(201).send({
+      status: "success",
+      message: "Pet adopted",
+      payload: newAdoption._id,
+    });
+  } catch (error) {
+    res.status(500).send({
       status: "error",
-      message: "Pet is already adopted",
+      message: `Internal Server Error: ${error}`,
       payload: [],
     });
-  user.pets.push(pet._id);
-  await usersService.update(user._id, { pets: user.pets });
-  await petsService.update(pet._id, { adopted: true, owner: user._id });
-  const newAdoption = await adoptionsService.create({
-    owner: user._id,
-    pet: pet._id,
-  });
-  res.status(201).send({
-    status: "success",
-    message: "Pet adopted",
-    payload: newAdoption._id,
-  });
+  }
 };
 
 export default {
