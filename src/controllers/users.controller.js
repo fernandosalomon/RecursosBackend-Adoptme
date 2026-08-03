d276ba43ca9ef894717a9ef29f78d4169a484fa9
fernandosalomon <fernandosalomon1992@gmail.com>
2026-08-03 11:18:04 -0300

    feat: standarized server response {status, message, payload} for users.ccontroller.js

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,35 +1,73 @@
-import { usersService } from "../services/index.js"
+import { usersService } from "../services/index.js";
 
-const getAllUsers = async(req,res)=>{
+const getAllUsers = async (req, res) => {
+  try {
     const users = await usersService.getAll();
-    res.send({status:"success",payload:users})
-}
+    res.send({ status: "success", message: "Users retrieved", payload: users });
+  } catch (error) {
+    res.status(500).send({
+      status: "error",
+      message: `Internal Server Error: ${error}`,
+      payload: null,
+    });
+  }
+};
 
-const getUser = async(req,res)=> {
-    const userId = req.params.uid;
+const getUser = async (req, res) => {
+  const userId = req.params.uid;
+  try {
     const user = await usersService.getUserById(userId);
-    if(!user) return res.status(404).send({status:"error",error:"User not found"})
-    res.send({status:"success",payload:user})
-}
+    if (!user)
+      return res
+        .status(404)
+        .send({ status: "error", message: "User not found", payload: null });
+    res.send({ status: "success", message: "User retrieved", payload: user });
+  } catch (error) {
+    res.status(500).send({
+      status: "error",
+      message: `Internal Server Error: ${error}`,
+      payload: null,
+    });
+  }
+};
 
-const updateUser =async(req,res)=>{
-    const updateBody = req.body;
-    const userId = req.params.uid;
+const updateUser = async (req, res) => {
+  const updateBody = req.body;
+  const userId = req.params.uid;
+  try {
     const user = await usersService.getUserById(userId);
-    if(!user) return res.status(404).send({status:"error", error:"User not found"})
-    const result = await usersService.update(userId,updateBody);
-    res.send({status:"success",message:"User updated"})
-}
+    if (!user)
+      return res
+        .status(404)
+        .send({ status: "error", message: "User not found", payload: null });
+    const result = await usersService.update(userId, updateBody);
+    res.send({ status: "success", message: "User updated", payload: user._id });
+  } catch (error) {
+    res.status(500).send({
+      status: "error",
+      message: `Internal Server Error: ${error}`,
+      payload: null,
+    });
+  }
+};
 
-const deleteUser = async(req,res) =>{
+const deleteUser = async (req, res) => {
+  try {
     const userId = req.params.uid;
     const result = await usersService.getUserById(userId);
-    res.send({status:"success",message:"User deleted"})
-}
+    res.send({ status: "success", message: "User deleted", payload: result });
+  } catch (error) {
+    res.status(500).send({
+      status: "error",
+      message: `Internal Server Error: ${error}`,
+      payload: null,
+    });
+  }
+};
 
 export default {
-    deleteUser,
-    getAllUsers,
-    getUser,
-    updateUser
-}
+  deleteUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+};
